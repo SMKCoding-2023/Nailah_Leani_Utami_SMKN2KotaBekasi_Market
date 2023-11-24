955bc2594df784c9550feeade08897ff2b10ab19
nizepiple <nailahleani2006@gmail.com>
2023-11-24 14:40:15 +0700

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,30 +1,77 @@
+<script lang="ts" setup>
+import { useAuthStore } from "~/stores/auth";
+const authStore = useAuthStore();
+const isAuthenticated = ref();
+const router = useRouter();
+
+isAuthenticated.value = useCookie("access_token").value;
+
+const logout = async () => {
+  await authStore.logout();
+  const accessToken = useCookie("access_token");
+  const refreshToken = useCookie("refresh_token");
+  accessToken.value = null;
+  refreshToken.value = null;
+  setTimeout(() => {
+    isAuthenticated.value = useCookie("access_token").value;
+  }, 100);
+  router.push({
+    path: "/",
+  });
+};
+</script>
+
 <template>
   <!-- Main navigation container -->
   <nav
-    class="flex-no-wrap relative flex w-full items-center justify-between bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4">
+    class="flex-no-wrap relative flex w-full items-center justify-between bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4"
+  >
     <div class="flex w-full flex-wrap items-center justify-between px-3">
       <!-- Hamburger button for mobile view -->
       <button
         class="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-        type="button" data-te-collapse-init data-te-target="#navbarSupportedContent12"
-        aria-controls="navbarSupportedContent12" aria-expanded="false" aria-label="Toggle navigation">
+        type="button"
+        data-te-collapse-init
+        data-te-target="#navbarSupportedContent12"
+        aria-controls="navbarSupportedContent12"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <!-- Hamburger icon -->
         <span class="[&>svg]:w-7">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-7 w-7">
-            <path fill-rule="evenodd"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="h-7 w-7"
+          >
+            <path
+              fill-rule="evenodd"
               d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-              clip-rule="evenodd" />
+              clip-rule="evenodd"
+            />
           </svg>
         </span>
       </button>
 
       <!-- Collapsible navigation container -->
-      <div class="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
-        id="navbarSupportedContent12" data-te-collapse-item>
+      <div
+        class="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
+        id="navbarSupportedContent12"
+        data-te-collapse-item
+      >
         <!-- Logo -->
-        <img src="/images/icons/images.png" style="height: 50px" alt="TE Logo" loading="lazy" />
+        <img
+          src="/images/icons/images.png"
+          style="height: 50px"
+          alt="TE Logo"
+          loading="lazy"
+        />
         <!-- Left navigation links -->
-        <ul class="list-style-none mr-auto flex flex-col pl-0 lg:flex-row" data-te-navbar-nav-ref>
+        <ul
+          class="list-style-none mr-auto flex flex-col pl-0 lg:flex-row"
+          data-te-navbar-nav-ref
+        >
           <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
             <!-- Dashboard link -->
             <!-- <a
@@ -33,8 +80,11 @@
             data-te-nav-link-ref
             >Home</a
           > -->
-            <nuxt-link to="/"
-              class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400">Home</nuxt-link>
+            <nuxt-link
+              to="/"
+              class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
+              >Home</nuxt-link
+            >
           </li>
           <!-- Team link -->
           <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
@@ -44,8 +94,11 @@
             data-te-nav-link-ref
             >Cart</a
           > -->
-            <nuxt-link to="/cart"
-              class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400">Cart</nuxt-link>
+            <nuxt-link
+              to="/cart"
+              class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              >Cart</nuxt-link
+            >
           </li>
           <!-- Projects link -->
           <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
@@ -55,13 +108,26 @@
             data-te-nav-link-ref
             >Projects</a
           > -->
-            <nuxt-link to="/products"
-              class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400">Product</nuxt-link>
+            <nuxt-link
+              to="/products"
+              class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              >Product</nuxt-link
+            >
+            <NuxtLink
+              to="/login"
+              class="text-base bg-purple-500 px-6 py-2 text-white rounded-lg hover:bg-purple-500/80"
+              >Login</NuxtLink
+            >
+            <div
+              v.else
+              class="text-base cursor-pointer bg-grey-400 px-6 py-2 text-white rounded-lg hover:bg-grey-400/80"
+              @click="logout"
+            >
+              logout
+            </div>
           </li>
         </ul>
       </div>
-
-
     </div>
   </nav>
 </template>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,16 +1,23 @@
 <script setup>
-import navbar from '../components/Header.vue';
-import copyright from '../components/Footer.vue';
+import navbar from "../components/Header.vue";
+import copyright from "../components/Footer.vue";
+
+definePageMeta({
+  middleware: ["user-access"],
+});
 </script>
 <template>
   <section>
     <navbar />
-    <div class="container mx-auto"> <!-- Tambahkan class mx-auto di sini -->
+    <div class="container mx-auto">
+      <!-- Tambahkan class mx-auto di sini -->
       <div class="py-10 ml-20">
         <div class="justify-end gap-6">
           <div class="py-10 flex gap-6">
             <div class="w-[70%]">
-              <div class="flex justify-between items-center pb-7 border-b border-gray-300 mb-6">
+              <div
+                class="flex justify-between items-center pb-7 border-b border-gray-300 mb-6"
+              >
                 <h1 class="text-3xl font-medium">Shopping Cart</h1>
                 <p class="text-3xl font-medium">0 Items</p>
               </div>
@@ -23,7 +30,9 @@ import copyright from '../components/Footer.vue';
               <div class="flex flex-col gap-3 border-b border-gray-300 pb-4">
                 <div>
                   <div class="flex gap-4 items-center">
-                    <span class="text-limit limit-1 text-sm">Men's Streetwear Fashion</span>
+                    <span class="text-limit limit-1 text-sm"
+                      >Men's Streetwear Fashion</span
+                    >
                     <span class="text-sm font-semibold">$30</span>
                   </div>
                 </div>
@@ -33,14 +42,18 @@ import copyright from '../components/Footer.vue';
                 <span class="text-base font-bold">$30</span>
               </div>
 
-              <button class="bg-blue-600 text-white text-base font-bold w-full py-2 rounded-lg">Checkout</button>
+              <button
+                class="bg-blue-600 text-white text-base font-bold w-full py-2 rounded-lg"
+              >
+                Checkout
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <br><br><br>
-    <br><br><br>
+    <br /><br /><br />
+    <br /><br /><br />
     <copyright />
   </section>
 </template>
